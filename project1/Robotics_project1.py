cea92a5d335da63434b2fb9d4cb85267dc40995f
Lin Huang Ting <138973404+linB110@users.noreply.github.com>
2025-10-16 19:37:39 +0800

    Update Robotics_project1.py

--- project1/Robotics_project1.py
+++ project1/Robotics_project1.py
@@ -1,8 +1,8 @@
 import numpy as np
-import math
+import math as m
 import pandas as pd
 from forward_kinematics import DHmodel_to_transformation, Rmat_to_theta
-import math as m
+from inverse_kinematics import inverse_kinematics
 
 # test data (https://www.youtube.com/watch?v=FNuiNmoqaZM)
 # kinematics_table = pd.DataFrame({
@@ -13,15 +13,18 @@ import math as m
 #     'theta': [0, -1.222, 1.588, 0, 0.785, 0],                 
 # })
 
+# limit of theta and length
+variable_seq = ["θ1", "θ2", "d3", "θ4", "θ5", "θ6"]
+limits = [160, 125, 30, 140, 100, 260]
+
 theta1, theta2, length3, theta4, theta5, theta6 = map(float, input(
     "please enter theta(-160~160), theta2(-125~125), length3(-30~30), "
     "theta4(-140~140), theta5(-100~100), theta6(-260~260): ").split())
 
-input_valid = [abs(theta1) <= 160, abs(theta2) <= 125, abs(length3) <= 30, abs(theta4) <= 140, abs(theta5) <= 100, abs(theta6) <= 260]
+input_valid = [abs(theta1) <= limits[0], abs(theta2) <= limits[1], abs(length3) <= limits[2], abs(theta4) <= limits[3], abs(theta5) <= limits[4], abs(theta6) <= limits[5]]
 names = ["theta1", "theta2", "length3", "theta4", "theta5", "theta6"]
 values = [theta1, theta2, length3, theta4, theta5, theta6]
 
-# 輸出結果
 for name, value, valid in zip(names, values, input_valid):
     if valid:
         print(f"{name} = {value}")
@@ -51,12 +54,43 @@ for i in range(6):
     T_forward = T_forward @ J_i
 
 rx, ry, rz = Rmat_to_theta(T_forward[0:3, 0:3])
+x, y, z = (T_forward[0:3, 3])
 
-#print(T_forward[0:3, 0:3])
+# forward kinematics result
+print("[n, o, a, p] : ")
+print(T_forward)
+print("\n")    
 
-print("[n, o, a, p] : \n")
-print(T_forward)    
+print("Cartesian point (x, y, z, φ, θ, ψ) : ")
+print(x, y, z, rx, ry, rz)
+print("\n")    
 
-print("output : \n")
-print(rx, ry, rz)
+# inverse kinematics result
+vals = list(map(float, input(
+    "please enter the cartesian point (Euler angle and x,y,z) : ").split()))
+if len(vals) != 16:
+    raise ValueError("You must enter exactly 16 numbers for a 4x4 matrix.")
 
+T = np.array(vals).reshape((4, 4))
+
+# make sure input of rotation matrix is orthogonal matrix
+R06 = T[:3, :3]
+U, _, Vt = np.linalg.svd(R06)
+R_orthonormal = U @ Vt
+T[:3, :3] = R_orthonormal
+
+result = inverse_kinematics(kinematics_table, T)
+for i in range(len(result)):
+    print("solution " + str(i))
+    print("corresponding variables (θ1, θ2, d3, θ4, θ5, θ6) : ")
+
+    for j in range(6):
+        if ( abs(result[i][j]) ) >= limits[j]:
+            print(variable_seq[j] + " is out of range !")
+    print(result[i])
+    print("\n")
+
+# forward and inverse kinematics can use for validate each other 
+# test = np.array(T_forward)
+# print("inverse solution")
+# print(inverse_kinematics(kinematics_table, pos_matrix=test))
